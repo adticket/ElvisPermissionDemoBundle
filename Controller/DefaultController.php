1bb6a2d71586f4c64ef87f8a2a98afb151668751
Markus Tacker <m@coderbyheart.de>
2013-02-07 22:54:44 +0100

    Verwende Modulrecht.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,15 +5,17 @@ namespace Adticket\Elvis\PermissionDemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/", name="permissiondemo_index")
      * @Template()
+     * @Secure("ELVISMODULE_PERMISSIONDEMO_VIEW")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array();
     }
 }
